cea5cdd26d3dbb15ce8cba741f3f17883f5c3631
Atu77l <kesharwaniatul9935@gmail.com>
2022-05-09 09:52:14 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+ function App() {
+  const [Users, fetchUsers] = useState([])
+  const getData = () => {
+    fetch('https://api.spacexdata.com/v4/launchpads')
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res)
+        fetchUsers(res)
+      })
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  return (
+    <>
+      <h2>React Fetch API Example</h2>
+      <ul>
+        {Users.map((item, i) => {
+          return <div>
+          <img src={item.images.large} className="image" alt="my image"/>
+          <h3 key={i}>{item.name}</h3>
+          <h4>{item.details}</h4>
+          <a href="https://api.spacexdata.com/v4/launchpads/`${item.launches[0]}'">launches1</a>
+          <a href="https://api.spacexdata.com/v4/launchpads/`${item.launches[0]}`">launches2</a>
+          <a href="https://api.spacexdata.com/v4/launchpads/{item.launches[0]}">launches3</a>
+          </div>
+        })}
+      </ul>
+    </>
+  )
+}
+
+export default App;
